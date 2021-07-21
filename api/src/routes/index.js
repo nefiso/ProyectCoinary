ec085eaa115e134dfd68e1f40ff575e6d9680069
NicoConstantin <nicoconstantin11@gmail.com>
2021-07-21 13:17:43 -0300

    starting

---
+++ api/src/routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+// Importar todos los routers;
+const router = Router();
+
+// Configurar los routers
+// router.use('/product', productRouter);
+
+module.exports = router;
